287d79b14af63bdf07507031e22fb008bafa71e3
Nicholas Thanasiu <nick.thanasiu@gmail.com>
2022-03-10 23:05:53 -0500

    Add HiveActionButtons

--- client/src/components/controls/HiveActions.tsx
+++ client/src/components/controls/HiveActions.tsx
@@ -1,5 +1,27 @@
+import styled from 'styled-components';
+import HiveActionButton from './HiveActionButton';
+
 function HiveActions() {
-    return <></>;
+    return (
+        <StyledHiveActions>
+            <HiveActionButton actionType="DELETE">
+                Delete
+            </HiveActionButton>
+            <HiveActionButton actionType="SHUFFLE" />
+            <HiveActionButton actionType="SUBMIT">
+                Enter
+            </HiveActionButton>
+        </StyledHiveActions>
+    );
 }
+
+const StyledHiveActions = styled.div`
+    text-align: center;
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    justify-content: center;
+    flex-wrap: nowrap;
+`;
 
 export default HiveActions;
