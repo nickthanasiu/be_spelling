55bebf9cb1e8e02234d9703392a4295e71c4e03e
Nicholas Thanasiu <nick.thanasiu@gmail.com>
2022-06-12 22:22:56 -0400

    API now returns wordCount, maxScore, and rankings

--- server/services/puzzles.ts
+++ server/services/puzzles.ts
@@ -1,66 +1,48 @@
+const puzzleData = require('../data/puzzles.json');
+
+// @TODO :: Refactor logic here. Should it all be in this file?
 const getDefaultPuzzle = () => {
-    // @TODO :: Should come from DB, not hard-coded
-    const defaultPuzzle = {
-        centerLetter: "o",
-        letters: [
-            "g",
-            "h",
-            "i",
-            "n",
-            "p",
-            "w"
-        ],
-        pangrams: [
-            "whooping",
-            "whopping"
-        ],
-        words: [
-            "going",
-            "gong",
-            "goon",
-            "goop",
-            "gown",
-            "gowning",
-            "hippo",
-            "hogging",
-            "honing",
-            "hoop",
-            "hooping",
-            "hoping",
-            "hopping",
-            "noggin",
-            "nohow",
-            "noon",
-            "ongoing",
-            "onion",
-            "opining",
-            "opinion",
-            "owing",
-            "owning",
-            "phoning",
-            "phono",
-            "pinion",
-            "pinioning",
-            "pogo",
-            "pogoing",
-            "pong",
-            "pooh",
-            "poop",
-            "pooping",
-            "popping",
-            "powwow",
-            "whoop",
-            "whop",
-            "winnow",
-            "winnowing",
-            "wino",
-            "woohoo",
-            "wooing",
-            "wowing"
-        ]
+    // @TODO :: Should come from DB, not hard-coded json
+    const { pangrams, words } = puzzleData;
+
+    // Calculate wordCount and add to puzzleData object
+    puzzleData.wordCount = pangrams.length + words.length;
+
+    // Pangram is worth its length plus 7
+    const calculatePangramScore = (pangram: string): number =>
+        pangram.length + 7;
+
+    // Four letter words are worth one point, while larger words are worth their length
+    const calculateWordScore = (word: string): number =>
+        word.length > 4 ? word.length : 1;
+
+    // Calculate maxScore and add to puzzleData object
+    let maxScore = 0;
+
+    words.forEach(word => maxScore += calculateWordScore(word));
+    pangrams.forEach(pangram => maxScore += calculatePangramScore(pangram));
+
+    puzzleData.maxScore = maxScore;
+
+    const percentToScore = (percentage: number) =>
+        Math.floor(maxScore * (percentage / 100));
+
+    // Generate rankings
+    const rankings = {
+        goodStart: percentToScore(2),
+        movingUp: percentToScore(5),
+        good: percentToScore(8),
+        solid: percentToScore(15),
+        nice: percentToScore(25),
+        great: percentToScore(40),
+        amazing: percentToScore(50),
+        genius: percentToScore(70),
+        queenBee: maxScore
     };
 
-    return defaultPuzzle;
+    puzzleData.rankings = rankings;
+
+    return puzzleData;
 };
 
 export default { getDefaultPuzzle };
