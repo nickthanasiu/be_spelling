e211d4e99515cea608da2b7819a9f08e5ee6ee91
Nicholas Thanasiu <nick.thanasiu@gmail.com>
2022-07-08 22:42:54 -0400

    configure and connect to mongodb

--- server/server.ts
+++ server/server.ts
@@ -3,9 +3,27 @@ import path from "path";
 import cors from "cors";
 import dotenv from "dotenv";
 import getRoutes from './routes';
+import mongoose from "mongoose";
 
 dotenv.config();
 
+// DB setup
+const mongodbUsername = process.env.MONGODB_USERNAME;
+const mongodbPassword = process.env.MONGODB_PASSWORD;
+
+mongoose.connect(`mongodb+srv://${mongodbUsername}:${mongodbPassword}@cluster0.3l6s1.mongodb.net/?retryWrites=true&w=majority`);
+
+const db = mongoose.connection;
+
+db.on('error', (error) => {
+    console.log(error);
+});
+
+db.once('open', () => {
+    console.log('Database Connected');
+});
+
+// Server setup
 const reactStaticFilesPathDevelopment = path.resolve(__dirname, "..", "client", "public");
 const reactStaticFilesPathProduction = path.resolve(__dirname, "public");
 
@@ -16,17 +34,15 @@ const reactAppPathProduction = path.resolve(__dirname, "public", "index.html");
 
 const REACT_APP_PATH = process.env.NODE_ENV === 'development' ? reactAppPathDevelopment : reactAppPathProduction;
 
-const PORT = process.env.PORT || 5000;
-
 const app = express();
 
-// CORS
 app.use(cors());
-
 app.use(express.json());
 
 // Serve the React static files after build
 app.use(express.static(REACT_STATIC_FILES_PATH));
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     // tslint:disable-next-line:no-console
