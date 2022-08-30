cc31aaf87144d78d6eebafd3f8b648499cd47578
Nicholas Thanasiu <nick.thanasiu@gmail.com>
2022-08-29 21:53:47 -0400

    Fix static files paths

--- server/loaders/express.ts
+++ server/loaders/express.ts
@@ -1,20 +1,24 @@
 import express from "express";
+import path from "path";
 import cors from "cors";
 import getRoutes from "../routes";
 import { Express } from "express";
-import { REACT_STATIC_FILES_PATH, REACT_APP_PATH } from "../configs/staticFiles";
 
 export default async (app: Express) => {
     app.use(cors());
     app.use(express.json());
 
     // Serve the React static files after build
-    app.use(express.static(REACT_STATIC_FILES_PATH));
+    app.use(express.static(
+        path.resolve(__dirname, "..", "public")
+    ));
 
     app.use('/api', getRoutes());
 
     // All other unmatched requests will return the React app
     app.get("/", (req, res) => {
-        res.sendFile(REACT_APP_PATH);
+        res.sendFile(
+            path.resolve(__dirname, "..", "public", "index.html")
+        );
     });
 };
