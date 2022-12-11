0b76f7333ed96af3f51b5fcd325b4e12aa87767a
Nicholas Thanasiu <nick.thanasiu@gmail.com>
2022-12-10 19:27:30 -0500

    Simplify server by removing loaders dir

--- server/index.ts
+++ server/index.ts
@@ -1,20 +1,53 @@
-import dotenv from "dotenv";
 import express from "express";
-import loaders from "./loaders";
+import path from "path";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import getRoutes from "./routes";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+// DB config
+const mongodbUsername = process.env.MONGODB_USERNAME;
+const mongodbPassword = process.env.MONGODB_PASSWORD;
 
-async function startServer() {
-    const app = express();
-
-    await loaders.init(app);
-
-    app.listen(PORT, () => {
+async function connectToDB() {
+    try {
+        await mongoose.connect(`mongodb+srv://${mongodbUsername}:${mongodbPassword}@cluster0.3l6s1.mongodb.net/?retryWrites=true&w=majority`);
+    
         // tslint:disable-next-line:no-console
-        console.log(`Server listening on ${PORT}`);
-    });
+        console.log('Database Connected');
+    } catch (error) {
+        // tslint:disable-next-line:no-console
+        console.log('Error connecting to database: ', error);
+    }
 }
 
-startServer();
+connectToDB();
+
+// Server config
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Serve the React static files after build
+app.use(express.static(
+    path.resolve(__dirname, "..", "public")
+));
+
+// Routes
+app.use('/api', getRoutes());
+
+app.get("/*", (req, res) => {
+    res.sendFile(
+        path.resolve(__dirname, "..", "public", "index.html")
+    );
+});
+
+app.listen(PORT, () => {
+    // tslint:disable-next-line:no-console
+    console.log(`Server listening on ${PORT}`);
+});
